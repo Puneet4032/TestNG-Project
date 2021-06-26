e2511ee6f120d151bd4034efe18ac62713da018b
Meliodas <Meliodas@Meliodas>
2021-06-25 22:13:15 -0400

    push

---
+++ Project/src/Project/Project.java
@@ -0,0 +1,42 @@
+package Project;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Project {
+	@Test
+	public void title() throws Exception{
+	 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Avneet\\Downloads\\chromedriver_win32\\chromedriver.exe");
+     ChromeDriver driver = new ChromeDriver();
+     
+     driver.get("http://automationpractice.com/index.php");
+     
+     String actualTitle = "My Store";
+     String expectedTitle = driver.getTitle();
+     
+     Assert.assertEquals(expectedTitle, actualTitle);
+     System.out.println("Test Case Passed");
+    
+	}
+	
+	@Test
+	public void logo() 
+	{
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Avneet\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	     ChromeDriver driver = new ChromeDriver();
+	     
+	     driver.get("http://automationpractice.com/index.php");
+	     
+		 WebElement logo = driver.findElement(By.className("logo")  );
+         Assert.assertEquals(true, logo.isDisplayed());
+         System.out.println("Test case passed");
+	}
+
+}
+
+
+
---
+++ Project/src/Project/counting.java
@@ -0,0 +1,54 @@
+package Project;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class counting {
+	@BeforeClass
+    void setUpClass() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Avneet\\Downloads\\chromedriver_win32\\chromedriver.exe");
+      
+    }
+    @Test
+    public void imagess() throws InterruptedException {
+        ChromeDriver driver =new ChromeDriver();
+
+        driver.get("http://automationpractice.com/index.php");
+        List<WebElement> listImages = driver.findElements(By.tagName("img"));
+        
+        int figures = 0;
+        for (WebElement image : listImages) { 
+            if ( image.isDisplayed()) { 
+                figures++;
+            }
+        }
+        System.out.println("No. of total displayed images: " + figures);
+        driver.close();
+    }
+    @Test
+    public void buttons() throws InterruptedException {
+        ChromeDriver driver =new ChromeDriver();
+
+        driver.get("http://automationpractice.com/index.php");
+        List<WebElement> listButtons = driver.findElements(By.tagName("button"));
+
+        int counts = 0;
+        for (WebElement button : listButtons) {
+            if(button.getText().equals("buttonName")) {
+                counts++;
+            }
+        }
+        System.out.println("No. of total displyed buttons: " + counts);
+
+        driver.close();
+    }
+
+
+}
